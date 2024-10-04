d2dbf86f7f3628ac38aa66390811879ce7d2fda4
tadaumi <145209013+tadaumi@users.noreply.github.com>
2024-10-04 23:06:12 +0900

    Add files via upload

--- handpose.mjs
+++ handpose.mjs
@@ -124,47 +124,7 @@ function getDefaultExportFromCjs (x) {
 	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
 }
 
-/**
- * Types of block
- * @enum {string}
- */
-var BlockType = {
-  /**
-   * Boolean reporter with hexagonal shape
-   */
-  BOOLEAN: 'Boolean',
-  /**
-   * A button (not an actual block) for some special action, like making a variable
-   */
-  BUTTON: 'button',
-  /**
-   * Command block
-   */
-  COMMAND: 'command',
-  /**
-   * Specialized command block which may or may not run a child branch
-   * The thread continues with the next block whether or not a child branch ran.
-   */
-  CONDITIONAL: 'conditional',
-  /**
-   * Specialized hat block with no implementation function
-   * This stack only runs if the corresponding event is emitted by other code.
-   */
-  EVENT: 'event',
-  /**
-   * Hat block which conditionally starts a block stack
-   */
-  HAT: 'hat',
-  /**
-   * Specialized command block which may or may not run a child branch
-   * If a child branch runs, the thread evaluates the loop block again.
-   */
-  LOOP: 'loop',
-  /**
-   * General reporter with numeric or string value
-   */
-  REPORTER: 'reporter'
-};
+
 var blockType = BlockType;
 var BlockType$1 = /*@__PURE__*/getDefaultExportFromCjs(blockType);
 
